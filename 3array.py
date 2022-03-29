ab732b701329c7cd34048df0f8f1a8ab2c0c60c1
MARIA FERNANDA <100495130+MAFE1697@users.noreply.github.com>
2022-03-28 22:03:45 -0500

    Add files via upload
    taller

--- 3array.py
+++ 3array.py
@@ -6,7 +6,6 @@ Created on Sun Mar 27 20:20:32 2022
 """
 
 nombres=[]
-apellido=[]
 apellidos=[]
 
 
@@ -15,11 +14,13 @@ for x in range(2):
    nombres.append(nom)
    
    ape1= input("Ingrese el  primer apellido: ")   
-   ape2= input("Ingrese el segundo apellido: ")
+   ape2=input("Ingrese el segundo apellido: ")
    apellidos.append([ape1,ape2])
    
 for x in range(2):
-    print(nombres[x],apellido[x][1],apellido[x][1])
+    print("Su nombre completo es: ", nombres[x],apellidos[x][0],apellidos[x] [1])
+  
+
    
   
             
